0493d69ff0ff68b529521273a0ab80b565a15b2f
Mohammed Rifas S <39994235+rifasm@users.noreply.github.com>
2020-05-16 12:12:41 +0530

    Read and Write Images

--- Basics/01_images.py
+++ Basics/01_images.py
@@ -4,7 +4,7 @@ import cv2
 img = cv2.imread("input_images/img_1.jpg", 0)       # Load a color image in greyscale
 
 cv2.imshow("Image", img)                            # Show the image on a frame
-k = cv2.waitKey(0)                                  # Wait for key press, since 64-bit machine, we AND the key with 0xFF
+k = cv2.waitKey(0) & 0xFF                           # Wait for key press, since 64-bit machine, we AND the key with 0xFF
 if k == 27:                                         # Check if ESC key is pressed
     cv2.destroyAllWindows()                         # Close all the open cv2 windows
 elif k == ord('s'):                                 # Check if key pressed is s
