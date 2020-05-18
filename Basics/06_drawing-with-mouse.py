f9e1228c52954c5e5a9290c4af50a55b5bad9bb6
Mohammed Rifas S <rifas2475@outlook.com>
2020-05-18 11:12:14 +0530

    Drawing with the mouse

---
+++ Basics/06_drawing-with-mouse.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+drawing = False                                             # true if mouse is pressed
+mode = True                                                 # if True, draw rectangle. Press 'm' to toggle to curve
+ix, iy = -1, -1
+
+
+# mouse callback function
+def draw_circle(event, x, y, flags, param):
+    global ix, iy, drawing, mode
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            if mode:
+                cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+            else:
+                cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        if mode:
+            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+        else:
+            cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+
+
+img = np.zeros((512, 512, 3), np.uint8)
+cv2.putText(img=img, text="Press m to toggle Mode", org=(10, 500), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1, color=(255, 255, 255), thickness=2, lineType=cv2.LINE_AA)
+cv2.namedWindow('Drawing')
+cv2.setMouseCallback('Drawing', draw_circle)
+
+while True:
+    cv2.imshow('Drawing', img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k == 27:
+        break
+
+cv2.destroyAllWindows()
