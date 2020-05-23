645fa0df49c014f5c84ffc87f65af2f7ef950871
Mohammed Rifas S <rifas2475@outlook.com>
2020-05-23 12:13:10 +0530

    Ostu's Binarization

--- Image Processing/05_otsu_binarization.py
+++ Image Processing/05_otsu_binarization.py
@@ -5,14 +5,14 @@ from matplotlib import pyplot as plt
 img = cv2.imread('../Media Files/input_images/img_7.jpg', 0)
 
 # global thresholding
-ret1, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+ret1, th1 = cv2.threshold(src=img, thresh=127, maxval=255, type=cv2.THRESH_BINARY)
 
 # Otsu's thresholding
-ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+ret2, th2 = cv2.threshold(src=img, thresh=0, maxval=255, type=cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 # Otsu's thresholding after Gaussian filtering
-blur = cv2.GaussianBlur(img, (5, 5), 0)
-ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+blur = cv2.GaussianBlur(src=img, ksize=(5, 5), sigmaX=0)
+ret3, th3 = cv2.threshold(src=blur, thresh=0, maxval=255, type=cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 # plot all the images and their histograms
 images = [img, 0, th1,
