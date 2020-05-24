8da31b7ead81e08d74fd5333c2f86a49bcc81ced
Mohammed Rifas S <rifas2475@outlook.com>
2020-05-24 14:10:10 +0530

    Translation

--- Image Processing/07_geometric_transformations.py
+++ Image Processing/07_geometric_transformations.py
@@ -12,10 +12,23 @@ img = cv2.imread('../Media Files/input_images/img_2.jpg')
 
 res = cv2.resize(src=img, dsize=None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 
-cv2.imshow("Result 1", res)
+cv2.imshow("Scaling Result 1", res)
 
 height, width = img.shape[:2]
 res = cv2.resize(src=img, dsize=(2 * width, 2 * height), interpolation=cv2.INTER_LINEAR)
-cv2.imshow("Result 2", res)
+cv2.imshow("Scaling Result 2", res)
 
 cv2.waitKey(0)
+
+# TRANSLATION
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+rows, cols = img.shape
+
+# shift of (100, 50) -> (cols to shift, rows to shift)
+M = np.float32([[1, 0, 100], [0, 1, 50]])
+dst = cv2.warpAffine(src=img, M=M, dsize=(cols, rows))
+
+cv2.imshow('Translation Result', dst)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
