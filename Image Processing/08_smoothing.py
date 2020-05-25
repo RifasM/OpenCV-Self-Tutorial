1534c4d5fa6469cfa81c70923790688b53e39c69
Mohammed Rifas S <rifas2475@outlook.com>
2020-05-25 11:10:19 +0530

    Image Blurring

--- Image Processing/08_smoothing.py
+++ Image Processing/08_smoothing.py
@@ -13,3 +13,28 @@ plt.xticks([]), plt.yticks([])
 plt.subplot(122), plt.imshow(dst), plt.title('Averaging')
 plt.xticks([]), plt.yticks([])
 plt.show()
+
+# Image Blurring (Image Smoothing)
+# 1. Averaging
+"""
+This is done by convolving the image with a normalized box filter.
+It simply takes the average of all the pixels under kernel area 
+and replaces the central element with this average. 
+This is done by the function cv2.blur() or cv2.boxFilter().
+
+If you don’t want to use a normalized box filter, 
+use cv2.boxFilter() and pass the argument normalize=False to the function.
+"""
+blur = cv2.blur(src=img, ksize=(5, 5))
+boxFilterFalse = cv2.boxFilter(src=img, ddepth=0, ksize=(5, 5), normalize=False)
+boxFilterTrue = cv2.boxFilter(src=img, ddepth=0, ksize=(5, 5), normalize=True)
+
+plt.subplot(221), plt.imshow(img), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(222), plt.imshow(blur), plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.subplot(223), plt.imshow(boxFilterFalse), plt.title('Box Filter False')
+plt.xticks([]), plt.yticks([])
+plt.subplot(224), plt.imshow(boxFilterTrue), plt.title('Box Filter True')
+plt.xticks([]), plt.yticks([])
+plt.show()
