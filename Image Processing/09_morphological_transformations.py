63c05cff55c4779d6bcea0c77cce5ed0dd48d692
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-01 13:54:32 +0530

    Opening

--- Image Processing/09_morphological_transformations.py
+++ Image Processing/09_morphological_transformations.py
@@ -9,6 +9,7 @@ Two basic morphological operators are Erosion and Dilation.
 Then its variant forms like Opening, Closing, Gradient etc also comes into play. 
 """
 img = cv2.imread('../Media Files/input_images/img_11.jpg', 0)
+kernel = np.ones((5, 5), np.uint8)
 cv2.imshow("Original", img)
 
 # EROSION
@@ -23,7 +24,6 @@ the size of kernel.
 So the thickness or size of the foreground object decreases or simply white region 
 decreases in the image. 
 """
-kernel = np.ones((5, 5), np.uint8)
 erosion = cv2.erode(src=img, kernel=kernel, iterations=1)
 cv2.imshow("Erosion", erosion)
 
@@ -42,5 +42,14 @@ dilation = cv2.dilate(src=img, kernel=kernel, iterations=1)
 erosion_dilation = cv2.dilate(src=img, kernel=kernel, iterations=1)
 cv2.imshow("Dilation", dilation)
 cv2.imshow("Erosion followed by Dilation", erosion_dilation)
+
+cv2.waitKey(0)
+
+# OPENING
+"""
+Opening is just another name of erosion followed by dilation. It is useful in removing noise
+"""
+opening = cv2.morphologyEx(src=img, op=cv2.MORPH_OPEN, kernel=kernel)
+cv2.imshow("Opening", opening)
 
 cv2.waitKey(0)
