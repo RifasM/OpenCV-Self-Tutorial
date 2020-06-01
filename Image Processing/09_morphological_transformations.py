c677e249ace0f067d0be8394e4adac9311086a6b
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-01 13:47:09 +0530

    Erosion

--- Image Processing/09_morphological_transformations.py
+++ Image Processing/09_morphological_transformations.py
@@ -12,6 +12,17 @@ img = cv2.imread('../Media Files/input_images/img_11.jpg', 0)
 cv2.imshow("Original", img)
 
 # EROSION
+"""
+The basic idea of erosion is just like soil erosion only, 
+it erodes away the boundaries of foreground object
+A pixel in the original image (either 1 or 0) will be considered 1 only if all the pixels 
+under the kernel is 1, otherwise it is eroded (made to zero).
+
+So what happens is that, all the pixels near boundary will be discarded depending upon 
+the size of kernel. 
+So the thickness or size of the foreground object decreases or simply white region 
+decreases in the image. 
+"""
 kernel = np.ones((5, 5), np.uint8)
 erosion = cv2.erode(img, kernel, iterations=1)
 cv2.imshow("Erosion", erosion)
