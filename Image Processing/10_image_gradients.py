2417766fb18a4e1e5ad7c0b0c22148af3497e730
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-07 19:27:57 +0530

    ddtypes

--- Image Processing/10_image_gradients.py
+++ Image Processing/10_image_gradients.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import re
+import matplotlib.pyplot as plt
 
 """
 OpenCV provides three types of gradient filters or High-pass filters, Sobel, Scharr and Laplacian.
@@ -10,3 +12,24 @@ OpenCV provides three types of gradient filters or High-pass filters, Sobel, Sch
 - Laplacian Derivative calculates the Laplacian of the image given by the relation, 
   \Delta src = \frac{\partial ^2{src}}{\partial x^2} + \frac{\partial ^2{src}}{\partial y^2}
 """
+
+img = cv2.imread('../Media Files/input_images/img_12.jpg', 0)
+
+# Print all available ddtypes
+# ninghang.blogspot.com/2012/11/list-of-mat-type-in-opencv.html
+[print(d, ":", getattr(cv2, d)) for d in [i for i in dir(cv2) if re.match('^(CV_).*', i)]]
+
+laplacian = cv2.Laplacian(src=img, ddepth=cv2.CV_64F, ksize=5)
+sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
+sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
+
+plt.subplot(2, 2, 1), plt.imshow(img, cmap='gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 2), plt.imshow(laplacian, cmap='gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 3), plt.imshow(sobelx, cmap='gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2, 2, 4), plt.imshow(sobely, cmap='gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+plt.show()
