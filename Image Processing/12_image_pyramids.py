293e1234cf18d865c0a18d2ddc4b5dd179f51020
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-08 17:59:52 +0530

    Blend

--- Image Processing/12_image_pyramids.py
+++ Image Processing/12_image_pyramids.py
@@ -18,3 +18,61 @@ There are two kinds of Image Pyramids.
     They are used in image compression. A level in Laplacian Pyramid is formed by the difference between that 
     level in Gaussian Pyramid and expanded version of its upper level in Gaussian Pyramid. 
 """
+
+# Image Blending using Pyramids
+
+A = cv2.imread('../Media Files/input_images/img_14.jpg')
+B = cv2.imread('../Media Files/input_images/img_15.jpg')
+
+# generate Gaussian pyramid for A
+G = A.copy()
+gpA = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gpA.append(G)
+
+# generate Gaussian pyramid for B
+G = B.copy()
+gpB = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    gpB.append(G)
+
+# generate Laplacian Pyramid for A
+lpA = [gpA[5]]
+for i in range(5, 0, -1):
+    GE = cv2.pyrUp(gpA[i])
+    L = cv2.subtract(gpA[i - 1], GE)
+    lpA.append(L)
+
+# generate Laplacian Pyramid for B
+lpB = [gpB[5]]
+for i in range(5, 0, -1):
+    GE = cv2.pyrUp(gpB[i])
+    L = cv2.subtract(gpB[i - 1], GE)
+    lpB.append(L)
+
+# Now add left and right halves of images in each level
+LS = []
+for la, lb in zip(lpA, lpB):
+    rows, cols, dpt = la.shape
+    ls = np.hstack((la[:, 0:cols // 2], lb[:, cols // 2:]))
+    LS.append(ls)
+
+# now reconstruct
+ls_ = LS[0]
+for i in range(1, 6):
+    ls_ = cv2.pyrUp(ls_)
+    ls_ = cv2.add(ls_, LS[i])
+
+# image with direct connecting each half
+print(A.shape)
+real = np.hstack((A[:, :A.shape[0]//2], B[:, B.shape[0]//2:]))
+
+cv2.imshow("Pyramid Blend", ls_)
+cv2.imshow("Direct Blend", real)
+
+cv2.waitKey(0)
+
+cv2.imwrite('../Media Files/output_images/Pyramid_blending2.jpg', ls_)
+cv2.imwrite('../Media Files/output_images/Direct_blending.jpg', real)
