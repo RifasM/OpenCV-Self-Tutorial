2f2698b9c0c30b21b12a567444fe232bfaa05210
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-15 20:06:36 +0530

    Bounding Rectangle

--- Image Processing/13_contours/13_2_contour_features.py
+++ Image Processing/13_contours/13_2_contour_features.py
@@ -79,7 +79,7 @@ contours, _ = cv2.findContours(thresh, 1, 2)
 
 cnt = contours[0]
 
-hull = cv2.convexHull(points=cnt)          # returnPoints = True (default)
+hull = cv2.convexHull(points=cnt)  # returnPoints = True (default)
 print("Default Hull:\n", hull)
 hull = cv2.drawContours(col_img.copy(), hull, -1, (0, 127, 127), 5)
 cv2.imshow("Default Hull (Return Points True)", hull)
@@ -88,7 +88,6 @@ hull = cv2.convexHull(points=cnt, returnPoints=False)
 print("Return Points False Hull:\n", hull)
 # These are the indices of corresponding points in contours
 
-print(len(contours[0]), len(contours[1]))
 imcopy = col_img.copy()
 hull_img = np.ones_like(img)
 for i in hull:
@@ -99,3 +98,50 @@ k = cv2.isContourConvex(cnt)
 print("Is Contour Convex?", k)
 
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Bounding Rectangle
+img = cv2.imread('../../Media Files/input_images/img_18.jpg')
+col_img = img.copy()
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Original Image", col_img)
+_, thresh = cv2.threshold(img, 127, 255, 0)
+contours, _ = cv2.findContours(thresh, 1, 2)
+
+cnt = contours[0]
+
+# Straight Bounding Rectangle
+x, y, w, h = cv2.boundingRect(array=cnt)
+img_copy = cv2.rectangle(img=col_img.copy(), pt1=(int(x), int(y)), pt2=(int(x + w), int(y + h)), color=(255, 0, 0),
+                         thickness=2)
+cv2.imshow("Normal Bounding rectangle", img_copy)
+
+# Rotated Rectangle
+rect = cv2.minAreaRect(points=cnt)
+box = cv2.boxPoints(box=rect)
+box = np.int0(box)
+img_copy = cv2.drawContours(col_img.copy(), [box], 0, (0, 0, 255), 2)
+cv2.imshow("Rotated Bounding rectangle", img_copy)
+
+# Minimum Enclosing Circle
+(x, y), radius = cv2.minEnclosingCircle(points=cnt)
+center = (int(x), int(y))
+img_copy = cv2.circle(img=col_img.copy(), center=center, radius=int(radius), color=(0, 255, 0), thickness=2)
+cv2.imshow("Minimum Enclosing Circle", img_copy)
+
+# Ellipse
+ellipse = cv2.fitEllipse(points=cnt)
+img_copy = cv2.ellipse(img=col_img.copy(), box=ellipse, color=(0, 255, 0), thickness=2)
+cv2.imshow("Ellipse", img_copy)
+
+# Line
+rows, cols = img.shape[:2]
+[vx, vy, x, y] = cv2.fitLine(points=cnt, distType=cv2.DIST_L2, param=0, reps=0.01, aeps=0.01)
+lefty = int((-x * vy / vx) + y)
+righty = int(((cols - x) * vy / vx) + y)
+img_copy = cv2.line(img=col_img.copy(), pt1=(cols - 1, righty), pt2=(0, lefty), color=(0, 255, 0), thickness=2)
+cv2.imshow("Line", img_copy)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
