4f31785c758b89618d599b58c685ad259227d0d0
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-15 17:06:49 +0530

    Contour features

--- Image Processing/13_contours/13_2_contour_features.py
+++ Image Processing/13_contours/13_2_contour_features.py
@@ -1,13 +1,21 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('../../Media Files/input_images/img_16.jpg', 0)
+img = cv2.imread('../../Media Files/input_images/img_17.jpg', 0)
+img_copy = img.copy()
+cv2.imshow("Original Image", img)
 ret, thresh = cv2.threshold(img, 127, 255, 0)
 contours, hierarchy = cv2.findContours(thresh, 1, 2)
+img_copy = cv2.drawContours(img_copy, contours, -1, (0, 255, 0), 2)
+cv2.imshow("All Contours", img_copy)
+del img_copy
 
 cnt = contours[0]
+img_copy = img.copy()
+img_copy = cv2.drawContours(img_copy, [cnt], 0, (255, 0, 0), 5)
+cv2.imshow("1st Contour", img_copy)
 M = cv2.moments(array=cnt)
-print("Moments:\n", M)
+print("Moments of 1st Contour:\n", M)
 
 """
 From this moments, you can extract useful data like area, centroid etc. 
@@ -22,8 +30,15 @@ print("Centroid: (", cx, ", ", cy, ")")
 
 # Contour Area is given by M00
 area = cv2.contourArea(cnt)
-print(area)
+print("Area:", area)
 
 # Perimeter
+"""
+It is also called arc length. 
+It can be found out using cv2.arcLength() function. 
+Second argument specify whether shape is a closed contour (True = closed contour), or just a curve.
+"""
 perimeter = cv2.arcLength(cnt, True)
-print(perimeter)
+print("Arc Length (Perimeter):", perimeter)
+
+cv2.waitKey(0)
