532f26214acfb06f90a53a4845809ed47e20456e
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-15 17:20:21 +0530

    Approximation

--- Image Processing/13_contours/13_2_contour_features.py
+++ Image Processing/13_contours/13_2_contour_features.py
@@ -3,18 +3,16 @@ import numpy as np
 
 img = cv2.imread('../../Media Files/input_images/img_17.jpg')
 col_img = img.copy()
-img_copy = img.copy()
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Original Image", img)
 ret, thresh = cv2.threshold(img, 127, 255, 0)
 contours, hierarchy = cv2.findContours(thresh, 1, 2)
-img_copy = cv2.drawContours(img_copy, contours, -1, (0, 255, 0), 2)
+img_copy = cv2.drawContours(col_img.copy(), contours, -1, (0, 255, 0), 2)
 cv2.imshow("All Contours", img_copy)
-del img_copy
 
 cnt = contours[0]
-img_copy = col_img.copy()
-img_copy = cv2.drawContours(img_copy, [cnt], 0, (255, 0, 0), 2)
+img_copy = cv2.drawContours(col_img.copy(), [cnt], 0, (255, 0, 0), 2)
 cv2.imshow("1st Contour", img_copy)
 M = cv2.moments(array=cnt)
 print("Moments of 1st Contour:\n", M)
@@ -40,7 +38,19 @@ It is also called arc length.
 It can be found out using cv2.arcLength() function. 
 Second argument specify whether shape is a closed contour (True = closed contour), or just a curve.
 """
-perimeter = cv2.arcLength(cnt, True)
+perimeter = cv2.arcLength(curve=cnt, closed=True)
 print("Arc Length (Perimeter):", perimeter)
+
+# Contour Approximation
+epsilon_1 = 0.01 * perimeter
+epsilon_10 = 0.1 * perimeter
+
+approx_1 = cv2.approxPolyDP(curve=cnt, epsilon=epsilon_1, closed=True)
+approx_1 = cv2.drawContours(col_img.copy(), [approx_1], -1, (0, 255, 200), 2)
+cv2.imshow("Approximation 1%", approx_1)
+
+approx_10 = cv2.approxPolyDP(curve=cnt, epsilon=epsilon_10, closed=True)
+approx_10 = cv2.drawContours(col_img.copy(), [approx_10], -1, (0, 0, 255), 2)
+cv2.imshow("Approximation 10%", approx_10)
 
 cv2.waitKey(0)
