81301543dc72d820cc44e9032dc9f1fd087dd159
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-20 14:27:36 +0530

    Convexity Defects

--- Image Processing/13_contours/13_4_contour_more_functions.py
+++ Image Processing/13_contours/13_4_contour_more_functions.py
@@ -8,7 +8,18 @@ contours, hierarchy = cv2.findContours(thresh, 2, 1)
 cnt = contours[0]
 
 hull = cv2.convexHull(cnt, returnPoints=False)
-defects = cv2.convexityDefects(cnt, hull)
+# Remember we have to pass
+# returnPoints = False
+# while finding convex hull, in order to find convexity defects.
+defects = cv2.convexityDefects(contour=cnt, convexhull=hull)
+print("Defects\n", defects)
+"""
+It returns an array where each row contains these values - 
+[ start point, end point, farthest point, approximate distance to farthest point ]. 
+
+We draw a line joining start point and end point, then draw a circle at the farthest point. 
+Remember first three values returned are indices of cnt. So we have to bring those values from cnt.
+"""
 
 for i in range(defects.shape[0]):
     s, e, f, d = defects[i, 0]
@@ -18,6 +29,6 @@ for i in range(defects.shape[0]):
     cv2.line(img, start, end, [0, 255, 0], 2)
     cv2.circle(img, far, 5, [0, 0, 255], -1)
 
-cv2.imshow('img', img)
+cv2.imshow('Result', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
