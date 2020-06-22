c059bdd05761d3f350d9eb996d3aa59d12464c1c
Mohammed Rifas S <rifas2475@outlook.com>
2020-06-22 10:20:32 +0530

    RETR_LIST
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- Image Processing/13_contours/13_5_contour_hierarchy.py
+++ Image Processing/13_contours/13_5_contour_hierarchy.py
@@ -1,5 +1,17 @@
 import cv2
 import numpy as np
+import re
+
+print("Available modes for contour: ")
+[print("\t", d, ":", getattr(cv2, d)) for d in [i for i in dir(cv2) if re.match('^(RETR_).*', i)]]
+
+print("\nAvailable methods for contour: ")
+[print("\t", d, ":", getattr(cv2, d)) for d in [i for i in dir(cv2) if re.match('^(CHAIN_).*', i)]]
+
+img = cv2.imread('../../Media Files/input_images/img_3.jpg')
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(img, 127, 255, 0)
 
 """
 So each contour has its own information regarding what hierarchy it is, 
@@ -14,4 +26,17 @@ OpenCV represents it as an array of four values :
 
 If there is no child or parent, that field is taken as -1
 If there are no values, it is set to -1
+
+Refer:
+https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contours_hierarchy/py_contours_hierarchy.html#hierarchy-representation-in-opencv
 """
+
+#  Retrieval Modes
+# RETR_LIST
+"""
+ It simply retrieves all the contours, but doesn’t create any parent-child relationship. 
+ Parents and kids are equal under this rule, and they are just contours. ie they all belongs to same hierarchy level.
+ This is the good choice to use in your code, if you are not using any hierarchy features.
+"""
+contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_NONE)
+print("\nHierarchy for RETR_LIST:\n", hierarchy)
