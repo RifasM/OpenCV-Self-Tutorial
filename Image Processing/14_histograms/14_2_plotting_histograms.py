4b9b0717ee816e33164c17fd50796aa0c601e382
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-07 19:27:37 +0530

    MatplotLib RGB Histogram
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- Image Processing/14_histograms/14_2_plotting_histograms.py
+++ Image Processing/14_histograms/14_2_plotting_histograms.py
@@ -8,7 +8,18 @@ Long Way : use OpenCV drawing functions
 """
 
 # Using Matplotlib
-img = cv2.imread('../../Media Files/input_images/img_5.jpg', 0)
-plt.hist(x=img.ravel(), bins=256, range=[0, 256])
+img = cv2.imread('../../Media Files/input_images/img_5.jpg')
+grey_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+plt.hist(x=grey_img.ravel(), bins=256, range=[0, 256])
 plt.title("Matplotlib Histogram")
+plt.show()
+
+# OR
+
+color = ('b', 'g', 'r')
+for i, col in enumerate(color):
+    histr = cv2.calcHist([img], [i], None, [256], [0, 256])
+    plt.plot(histr, color=col)
+    plt.xlim([0, 256])
+plt.title("BGP Histogram Plot")
 plt.show()
