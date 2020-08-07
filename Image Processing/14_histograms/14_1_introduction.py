fd6362d29e4d1c038862c60a5de00d2d0209da40
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-07 18:37:50 +0530

    Histogram Introduction
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

---
+++ Image Processing/14_histograms/14_1_introduction.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+"""
+ It is a plot with pixel values (ranging from 0 to 255, not always) 
+ in X-axis and corresponding number of pixels in the image on Y-axis.
+ Remember, this histogram is drawn for grayscale image, not color image).
+ 
+ Histogram Terminology
+ - BINS
+ - DIMS
+ - RANGE
+"""
