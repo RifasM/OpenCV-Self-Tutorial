1d3e2b62f04ccaf2ab23ad54c7160dae5a355b67
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-07 19:04:34 +0530

    Histogram Introduction
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- Image Processing/14_histograms/14_1_introduction.py
+++ Image Processing/14_histograms/14_1_introduction.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
  It is a plot with pixel values (ranging from 0 to 255, not always) 
@@ -7,7 +8,47 @@ import numpy as np
  Remember, this histogram is drawn for grayscale image, not color image).
  
  Histogram Terminology
- - BINS
- - DIMS
- - RANGE
+ - DIMS - It is the number of parameters for which we collect the data. 
+          In this case, we collect data regarding only one thing, 
+          intensity value. So here it is 1.
+ - BINS - It is the number of subdivisions in each dim.
+ - RANGE - It is the range of intensity values you want to measure. 
+           Normally, it is [0,256], ie all intensity values.
 """
+
+"""
+- Histogram Calculation in OpenCV
+    SYNTAX: cv2.calcHist(images, channels, mask, histSize, ranges[, hist[, accumulate]])
+    
+    images : it is the source image of type uint8 or float32. 
+            it should be given in square brackets, ie, “[img]”.
+    channels : it is also given in square brackets. 
+            It the index of channel for which we calculate histogram. 
+            For example, if input is grayscale image, its value is [0]. 
+            For color image, you can pass [0],[1] or [2] to calculate 
+            histogram of blue,green or red channel respectively.
+    mask : mask image. To find histogram of full image, it is given as “None”.
+            But if you want to find histogram of particular region of image, 
+            you have to create a mask image for that and give it as mask.
+    histSize : this represents our BIN count. Need to be given in 
+            square brackets. For full scale, we pass [256].
+    ranges : this is our RANGE. Normally, it is [0,256].
+"""
+img = cv2.imread('../../Media Files/input_images/img_5.jpg')
+blue_hist = cv2.calcHist(images=[img], channels=[0], mask=None, histSize=[256],
+                         ranges=[0, 256])
+green_hist = cv2.calcHist(images=[img], channels=[1], mask=None, histSize=[256],
+                          ranges=[0, 256])
+red_hist = cv2.calcHist(images=[img], channels=[2], mask=None, histSize=[256],
+                        ranges=[0, 256])
+plt.subplot(2, 2, 1), plt.plot(blue_hist, color="b"), plt.title("Blue Histogram")
+plt.subplot(2, 2, 2), plt.plot(green_hist, color="g"), plt.title("Green Histogram")
+plt.subplot(2, 2, 3), plt.plot(red_hist, color="r"), plt.title("Red Histogram")
+plt.subplot(2, 2, 4), plt.plot(blue_hist, color="b")
+plt.subplot(2, 2, 4), plt.plot(green_hist, color="g")
+plt.subplot(2, 2, 4), plt.plot(red_hist, color="r"), plt.title("Overlay")
+plt.show()
+
+grey_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+grey_hist = cv2.calcHist(images=[grey_image], channels=[0], mask=None, histSize=[256],
+                         ranges=[0, 256])
