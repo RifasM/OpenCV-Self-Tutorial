64f743bdb9126ca88e9fcb482f58c6ef749caba2
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-19 21:05:03 +0530

    Histogram Equalisation
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- Image Processing/14_histograms/14_3_histogram_equalization.py
+++ Image Processing/14_histograms/14_3_histogram_equalization.py
@@ -60,3 +60,25 @@ res = np.hstack((img, equ))  # stacking images side-by-side
 cv2.imshow('Result', res)
 
 cv2.waitKey(0)
+
+# CLAHE (Contrast Limited Adaptive Histogram Equalization)
+"""
+ In this, image is divided into small blocks called “tiles” 
+ (tileSize is 8x8 by default in OpenCV). Then each of these 
+ blocks are histogram equalized as usual. So in a small area,
+  histogram would confine to a small region (unless there 
+  is noise). If noise is there, it will be amplified. 
+  To avoid this, contrast limiting is applied. If any 
+  histogram bin is above the specified contrast limit 
+  (by default 40 in OpenCV), those pixels are clipped and
+ distributed uniformly to other bins before applying 
+ histogram equalization. After equalization, to remove 
+ artifacts in tile borders, bilinear interpolation is applied.
+"""
+# create a CLAHE object (Arguments are optional).
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+cl1 = clahe.apply(img)
+
+cv2.imshow('CLAHE Result', cl1)
+
+cv2.waitKey(0)
