7f085b37ff145ff66adb620f70fc323937f7b8f8
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-19 20:58:13 +0530

    Histogram Equalisation
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- Image Processing/14_histograms/14_3_histogram_equalization.py
+++ Image Processing/14_histograms/14_3_histogram_equalization.py
@@ -16,5 +16,32 @@ plt.legend(('cdf', 'histogram'), loc='upper left')
 plt.show()
 
 """
-
+You can see histogram lies in brighter region. 
+We need the full spectrum. 
+For that, we need a transformation function which 
+maps the input pixels in brighter region to output 
+pixels in full region. That is what histogram 
+equalization does.
 """
+
+# Now we find the minimum histogram value (excluding 0)
+# and apply the histogram equalization equation
+cdf_m = np.ma.masked_equal(cdf, 0)
+cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+
+# Now we have the look-up table that gives us the
+# information on what is the output pixel value
+# for every input pixel value. So we just apply the transform.
+img2 = cdf[img]
+
+hist2, _ = np.histogram(img2.flatten(), 256, [0, 256])
+
+cdf2 = hist2.cumsum()
+cdf_normalized2 = cdf2 * hist2.max() / cdf2.max()
+
+plt.plot(cdf_normalized2, color='b')
+plt.hist(img2.flatten(), 256, [0, 256], color='r')
+plt.xlim([0, 256])
+plt.legend(('cdf', 'histogram'), loc='upper left')
+plt.show()
