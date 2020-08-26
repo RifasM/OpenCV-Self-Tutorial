bdce77d5d4bb82dbb68b3037129e9ebe90b205d4
Mohammed Rifas S <rifas2475@outlook.com>
2020-08-26 17:52:19 +0530

    2D histograms
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

---
+++ Image Processing/14_histograms/14_4_2d_histograms.py
@@ -0,0 +1,15 @@
+"""
+In the first article, we calculated and plotted one-dimensional histogram.
+It is called one-dimensional because we are taking only one feature into
+our consideration, ie grayscale intensity value of the pixel.
+But in two-dimensional histograms, you consider two features.
+Normally it is used for finding color histograms where two
+features are Hue & Saturation values of every pixel.
+"""
+
+import cv2
+
+img = cv2.imread("../../Media Files/input_images/img_6.jpg")
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+hist = cv2.calcHist([hsv], None, [100, 256], [0, 100, 0, 256])
